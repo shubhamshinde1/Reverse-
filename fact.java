b3e1380110f9f19ebb577ba75aeb3f647beca761
shubhamshinde1 <41343519+shubhamshinde1@users.noreply.github.com>
2018-07-25 23:31:25 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+ class Fact
+{
+  public static void main(String args[])
+  {
+     int i,fact=1,num=5;
+	for(i=1;i<=num;i++)
+	{
+	  fact=fact*i;
+	}
+	System.out.println("Factorial of Num= "+fact);
+   }
+}
